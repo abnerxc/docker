294b3f4ea96735a8d775a16903e77aaed1776592
xuchen <xuchen@icsoc.net>
2019-12-25 12:16:47 +0800

    init
    Change-Id: Ib5d38c9cccbed09c061aa8373925094942f647d6

--- install.sh
+++ install.sh
@@ -16,8 +16,9 @@ function main(){
 		case $number in
 		1)
 		echo "docker install starting" \
+		&& yum install -y yum-utils \
 		&& yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo \
-		&& yum install -y yum-utils device-mapper-persistent-data lvm2 docker-ce \
+		&& yum install -y device-mapper-persistent-data lvm2 docker-ce \
 		&& yum makecache fast \
 		&& service docker start \
 		&& curl -L https://get.daocloud.io/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
