ee4b2f3880daf82a149a96b64c2817366941c397
xuchen <abner510@126.com>
2021-09-26 16:20:02 +0800

    add

---
+++ ffmpeg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run -it -v /Users/abner/work/ffdev:/root --entrypoint='sh'  jrottenberg/ffmpeg:4.1-alpine 
+
+
+
+# ffmpeg -i 'https://apd-b3b3eaf0c45cf5becaba36384c9a9e8c.v.smtcdns.com/vipts.tc.qq.com/AXVcuzOjbGIDLwqA4V42i15rdkIi3tnUkEv-SseFJCIc/uwMROfz2r57AoaQXGdGnC2de6448_15NlFLR-6JgreBXkPz2/svp_50112/YtNkJg4J7twtzLf3-Toscfl7P5PPiNoOh28NvRUrj3WW-0aK-pnxb5ZyHuirdxeLrWqT2C29KC6Ru2bdvx_Hj9UecB-tFQvpnOXt31UjC13zuu2f24UAsfNZzFw6VibdLd0LZwCwM1cJTz6VIU8wg2QLu4k_8GhLvYeXKqY13MN5M7p_3jptFQ/0385_gzc_1000102_0b53caaaaaaavuapd6b5xnq4aegdaaoaabca.f321002.ts.m3u8?ver=4' -c copy xuchen.mp4
