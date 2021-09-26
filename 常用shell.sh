4e2f99256cf980034e1a09100f1db0cfbe852224
xuchen <abner510@126.com>
2021-09-26 21:36:30 +0800

    add

--- 常用shell.sh
+++ 常用shell.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker run -it -v /Users/abner/Movies:/root --entrypoint='sh'  jrottenberg/ffmpeg:4.1-alpine 
+# 进入容器 docker attach jrottenberg/ffmpeg:4.1-alpine 
 # ffmpeg  -i 'https://new.iskcd.com/20210921/WQP04lpt/3509kb/hls/index.m3u8' -vcodec copy -acodec copy  zgys.mp4
