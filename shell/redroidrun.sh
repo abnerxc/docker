9906d235d5b36b8e5a1320859aabbfe231be76d0
xuchen <abner510@126.com>
2023-08-20 23:15:19 +0800

    add

--- shell/redroidrun.sh
+++ shell/redroidrun.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -itd --rm --privileged --pull always -v /data/redroid/dev_fs:/data -p 5555:5555 redroid/redroid:13.0.0_64only-latest
+# docker-compose 启动无效  ，教程文章https://blog.csdn.net/u012960155/article/details/129277193
+docker run -itd --rm --privileged --pull always --name redroid -v /data/redroid/dev_fs:/data -p 5555:5555 redroid/redroid:13.0.0_64only-latest ro.secure=0 androidboot.redroid_width=1080 androidboot.redroid_height=1920 androidboot.redroid_dpi=480
