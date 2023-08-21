70924becb958ecd1e3a1cb10958daf5c01aba220
xuchen <abner510@126.com>
2023-08-21 09:21:15 +0800

    add

--- shell/android_ubuntu.sh
+++ shell/android_ubuntu.sh
@@ -4,7 +4,8 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable" && \
 sudo apt-get -y install docker-ce && \
-sudo gpasswd -a ${USER} docker  && sudo mkdir -p /etc/docker && sudo systemctl daemon-reload && sudo systemctl restart docker && sudo systemctl enable docker &&  sudo apt-get upgrade && \
+sudo gpasswd -a ${USER} docker  && sudo mkdir -p /etc/docker && sudo systemctl daemon-reload && \
+sudo systemctl restart docker && sudo systemctl enable docker &&  sudo apt-get upgrade && \
 sudo echo '{ "registry-mirrors": ["https://l714mp7z.mirror.aliyuncs.com"] }' >  /etc/docker/daemon.json && \
 sudo curl https://ghproxy.com/https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
 sudo chmod +x /usr/local/bin/docker-compose && \
