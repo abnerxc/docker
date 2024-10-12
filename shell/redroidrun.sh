983ddc9d80106be5a31512404563c0c515d446b9
xuchen <abner510@126.com>
2024-10-13 00:51:35 +0800

    1

--- shell/redroidrun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# docker-compose 启动无效  ，教程文章https://blog.csdn.net/u012960155/article/details/129277193
-docker run -itd --rm --privileged --pull always --name redroid -v /data/redroid/dev_fs:/data -p 5555:5555 redroid/redroid:13.0.0_64only-latest ro.secure=0
